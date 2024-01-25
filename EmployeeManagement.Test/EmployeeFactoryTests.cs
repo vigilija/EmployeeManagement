b1711f1fc4f8570898acc6ed23569675eab81cb3
vigilija <vigilija.simkeviciute@gmail.com>
2024-01-25 19:03:05 +0100

    Added in range Employee Factory tests

--- EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -12,10 +12,36 @@ namespace EmployeeManagement.Test
             var employeeFactory = new EmployeeFactory();
 
             //Act
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Viktorija", "Smith");
 
             //Assert
             Assert.Equal(2500, employee.Salary);
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500()
+        {
+            //Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            //Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Viktorija", "Smith");
+
+            //Assert
+            Assert.True(employee.Salary >= 2500 && employee.Salary <= 3500, "Salary not in acceptable range.");
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500WithInRange()
+        {
+            //Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            //Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Viktorija", "Smith");
+
+            //Assert
+            Assert.InRange(employee.Salary, 2500, 3500);
         }
     }
 }
