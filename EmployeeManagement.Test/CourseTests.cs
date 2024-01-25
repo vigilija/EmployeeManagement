a2e606622bd27b91fbfcb88926ce7ad97f3f52f9
vigilija <vigilija.simkeviciute@gmail.com>
2024-01-25 18:00:48 +0100

    ConstructCourse test

---
+++ EmployeeManagement.Test/CourseTests.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Test
+{
+    public class CourseTests
+    {
+        [Fact]
+        public void CourseConstructor_ConstructCourse_IsNewMustBeTrue()
+        {
+            //nothing to Arrange
+
+            //Act
+            var course = new Course("Disaster Manegment 101");
+
+            //Assert
+            Assert.True(course.IsNew);
+        }
+    }
+}
