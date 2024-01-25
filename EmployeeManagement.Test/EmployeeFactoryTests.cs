0ca398ac003b4451d681f985b2ecd9df81137e94
vigilija <vigilija.simkeviciute@gmail.com>
2024-01-25 17:24:45 +0100

    Added Test project and Employee salary test.

---
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -0,0 +1,18 @@
+using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeFactoryTests
+    {
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+
+            Assert.Equal(2500, employee.Salary);
+        }
+    }
+}
