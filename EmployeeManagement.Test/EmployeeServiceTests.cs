cc71b1c25974b7e1d79c341ccd78750059bb1f2c
vigilija <vigilija.simkeviciute@gmail.com>
2024-01-26 15:25:05 +0100

    Testing CreateEmployee (internal) method

---
+++ EmployeeManagement.Test/EmployeeServiceTests.cs
@@ -0,0 +1,119 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+using EmployeeManagement.Services.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeServiceTests
+    {
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedFirstObligatoryCourse()
+        {
+            //Arrange
+            var employeeManagementTestDataRepository = 
+                new EmployeeManagementTestDataRepository();
+
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepository,
+                new EmployeeFactory());
+
+            var obligatoryCourse = employeeManagementTestDataRepository
+                .GetCourse(Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"));
+
+            //Act
+            var internalEmployee = employeeService
+                .CreateInternalEmployee("Brooklyn", "Cannon");
+
+            //Assert
+            Assert.Contains(obligatoryCourse, internalEmployee.AttendedCourses);
+        }
+
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedFirstObligatoryCourseWithPredicet()
+        {
+            //Arrange
+            var employeeManagementTestDataRepository =
+                new EmployeeManagementTestDataRepository();
+
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepository,
+                new EmployeeFactory());
+
+            //Act
+            var internalEmployee = employeeService
+                .CreateInternalEmployee("Brooklyn", "Cannon");
+
+            //Assert
+            Assert.Contains(internalEmployee.AttendedCourses,
+                course => course.Id == Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"));
+        }
+
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedSecondObligatoryCourseWithPredicet()
+        {
+            //Arrange
+            var employeeManagementTestDataRepository =
+                new EmployeeManagementTestDataRepository();
+
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepository,
+                new EmployeeFactory());
+
+            //Act
+            var internalEmployee = employeeService
+                .CreateInternalEmployee("Brooklyn", "Cannon");
+
+            //Assert
+            Assert.Contains(internalEmployee.AttendedCourses,
+                course => course.Id == Guid.Parse("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e"));
+        }
+
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_AttendedCoursesMustMatchObligatoryCourses()
+        {
+            //Arrange
+            var employeeManagementTestDataRepository =
+                new EmployeeManagementTestDataRepository();
+
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepository,
+                new EmployeeFactory());
+
+            var obligatoryCourses = employeeManagementTestDataRepository
+              .GetCourses(Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"),
+                          Guid.Parse("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e"));
+
+            //Act
+            var internalEmployee = employeeService
+                .CreateInternalEmployee("Brooklyn", "Cannon");
+
+            //Assert
+            Assert.Equal(internalEmployee.AttendedCourses, obligatoryCourses);
+        }
+
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_AttendedCoursesMustNotBeNew()
+        {
+            //Arrange
+            var employeeManagementTestDataRepository =
+                new EmployeeManagementTestDataRepository();
+
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepository,
+                new EmployeeFactory());
+
+            //Act
+            var internalEmployee = employeeService
+                .CreateInternalEmployee("Brooklyn", "Cannon");
+
+            //Assert
+            Assert.All(internalEmployee.AttendedCourses, course => Assert.False(course.IsNew));
+ 
+        }
+    }
+}
